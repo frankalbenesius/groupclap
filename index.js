ac26e6e50203f14472d85750e8d105a97f44c73c
Frank Albenesius <frankalbenesius@gmail.com>
2020-03-12 21:27:56 -0500

    Use PORT env var.

--- index.js
+++ index.js
@@ -12,6 +12,5 @@ io.on("connection", function(socket) {
   });
 });
 
-http.listen(3000, function() {
-  console.log("listening on *:3000");
-});
+const port = process.env.PORT || 5000;
+app.listen(port);
